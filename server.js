4e4e8fcbdb586ed84c27213844394b6446e05d51
ionox0 <ionox0@gmail.com>
2014-04-13 15:45:54 -0700

    folder setup, pg connection

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var express = require('express')
-  , app = express.createServer(express.logger())
-  , pg = require('pg').native
-  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
-  , start = new Date()
-  , port = process.env.PORT || 3000
-  , client;
+var express = require('express');
+var port = process.env.PORT || 3000;
 
+
+
+
+var pg = require('pg');
+var conString = "postgres://postgres:1234@localhost/ian";
+
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * FROM samples', function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[1]);
+    //output: 1
+  });
+});
