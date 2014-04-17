827a2e665816d1a8cf1f3f9bf7060fad2c170cb5
ionox0 <ionox0@gmail.com>
2014-04-17 15:16:27 -0700

    more test coverage

--- server.js
+++ server.js
@@ -5,38 +5,56 @@ var app = express();
 app.use(express.static(__dirname + '/build'));
 
 var pg = require('pg');
-var conString = "postgres://postgres:1234@localhost/ian";
+var conString = "postgres://postgres:1234@localhost/aba";
 var client = new pg.Client(conString);
 client.connect();
 
 
 var geneRouter = require('./api/routers/geneRouter')
-app.get('/api/v1/:structure_id', getSample);
+app.get('/api/v1/probes/:gene_id', getProbe);
+app.get('/api/v1/exprVals/:probe_id', getExprVals);
+app.get('/api/v1/samples/:structure_id', getSample);
 
 
+function getProbe(req, res){
+  var id = req.params.gene_id;
+  client.query('SELECT * FROM probes WHERE gene_id=' + id, function(err, probes) {
+    //call `done()` to release the client back to the pool
+    //done(); --not defined...
+    if(err) {
+      return console.error('error running query', err);
+    }
+    res.send(probes);
+  })
+;}
+
+
+function getExprVals(req, res){
+  var id = req.params.probe_id;
+  client.query('SELECT * FROM exprvals WHERE probe_id=' + id, function(err, exprVals) {
+    //call `done()` to release the client back to the pool
+    //done(); --not defined...
+    if(err) {
+      return console.error('error running query', err);
+    }
+    res.send(exprVals);
+  });
+}
 
 
 function getSample(req, res){
-
   var id = req.params.structure_id;
-  var that = this;
-
   client.query('SELECT * FROM samples WHERE structure_id=' + id, function(err, samples) {
     //call `done()` to release the client back to the pool
     //done(); --not defined...
     if(err) {
       return console.error('error running query', err);
     }
-    //console.log(sample.rows);
     res.send(samples);
   });
-
 }
 
 
-
-
-
 app.listen(port, function() {
-  console.log('Listening on:', port);
+  console.log('Listening on:', port, '\n');
 });
--- test/api_tests.js
+++ test/api_tests.js
@@ -1,21 +1,40 @@
 'use strict';
 //jshint unused:false
 
-var superagent = require('superagent');
-var chai = require('chai'),
-  expect = chai.expect,
-  should = chai.should();
-var app = require('../server').app;
+var superagent = require('superagent'),
+chai = require('chai'),
+expect = chai.expect,
+should = chai.should(),
+app = require('../server').app;
 
-describe('api tests', function(){
+describe('API TESTS \n', function(){
 
-  it('can get a sample\'s details', function(done){
-    superagent.get('localhost:3000/api/v1/4077').end(function(e, res){
-        console.log(res.body.rows);
-        expect(e).to.eql(null);
-        expect(res.body.rows[0].structure_id).to.eql(4077);
-        done();
+  it('can get a gene\'s probes', function(done){
+    superagent.get('localhost:3000/api/v1/probes/186').end(function(e, res){
+      expect(e).to.eql(null);
+      expect(res.body.rows[0].gene_symbol).to.eql('AGXT');
+      for (var i = 0; i < res.body.rows.length; i++)
+        console.log(res.body.rows[i].probe_id, res.body.rows[i].gene_symbol);
+      done();
     });
   });
 
+  it('can get a probes\'s expression values', function(done){
+    superagent.get('localhost:3000/api/v1/exprVals/1058685').end(function(e, res){
+      expect(e).to.eql(null);
+      expect(res.body.rows[0].probe_id).to.eql(1058685);
+      for (var i = 0; i < res.body.rows.length; i++)
+        console.log(res.body.rows[i].probe_id, res.body.rows[i].h1, res.body.rows[i].h2);
+      done();
+    });
+  });
+
+  it('can get a sample\'s details', function(done){
+    superagent.get('localhost:3000/api/v1/samples/4077').end(function(e, res){
+      expect(e).to.eql(null);
+      expect(res.body.rows[0].structure_id).to.eql(4077);
+      console.log(res.body.rows[0].structure_id, res.body.rows[0].structure_acronym, res.body.rows[0].mri_voxel_x);
+      done();
+    });
+  });
 });
